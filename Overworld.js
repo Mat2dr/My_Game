a1ee22147e764627c3d551d2617b4969829d0aaa
Mat2dr <dragovic.mathias@gmail.com>
2022-05-06 17:53:15 +0200

    Initial commit

---
+++ Overworld.js
@@ -0,0 +1,41 @@
+class Overworld {
+    constructor(config) {
+        this.element = config.element;
+        this.canvas = this.element.querySelector(".game-canvas");
+        this.ctx = this.canvas.getContext('2d');
+        this.map = null;
+    }
+
+    startGameLoop() {
+        const step = () => {
+            //Clear the canvas
+            this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+            //Draw lower layer
+            this.map.drawLowerImage(this.ctx);
+
+            //Draw GameObjects layer
+            Object.values(this.map.gameObjects).forEach(object => {
+                object.x += 0.02;
+                object.sprite.draw(this.ctx)
+            })
+
+            //Draw Uper layer
+            this.map.drawUpperImage(this.ctx);
+
+            requestAnimationFrame(() =>{
+                step();
+            })
+        }
+        step();
+    }
+
+    init() {
+        this.map = new OverworldMap(window.OverworldMaps.Kitchen)
+
+        this.startGameLoop();
+
+
+
+    }
+}
